1d0ba3fc52292acdf67ebefc6b6f599d9072125a
kevinstiebich <kevin.stiebich@gmail.com>
2026-07-23 16:13:13 +0200

    bonus von aufg 2 lösen

--- aufgabe2_bonus.c
+++ aufgabe2_bonus.c
@@ -4,16 +4,14 @@ int main() {
     int arr[5] = {1, 2, 3, 4, 5};
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    for (int i = 0; i < size; i++) {
-        if (i < size / 2) {
+    for (int i = 0; i < (size / 2); i++) {
             int safe = arr[i];
             arr[i] = arr[size - 1 - i];
             arr[size - 1 - i] = safe;
-        
-            printf("%d\n", arr[i]);
-        } else {
-            
-        }
+    }
+
+    for (int i = 0; i < size; i++) {
+        printf("%d\n", arr[i]);
     }
 
 

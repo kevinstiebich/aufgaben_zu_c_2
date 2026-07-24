5651d560f3cd39a7d8f56330e89bfa6acfc01283
kevinstiebich <kevin.stiebich@gmail.com>
2026-07-24 12:52:37 +0200

    lösen von Aufgabe 5

---
+++ aufgabe5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    //lässt den Benutzer einen Satz eingeben und speichert ihn in einem Array
+    char eingabe[50];
+    printf("Gebe einen Satz ein: ");
+    fgets(eingabe, sizeof(eingabe), stdin);
+    
+    //Entfernen aller Zeichen außer die Buchstaben a-z und füllt den Rest mit Nullbytes
+    int j = 0;
+    for (int i = 0; eingabe[i] != '\0'; i++) {
+        if (eingabe[i] >= 'a' && eingabe[i] <= 'z') {
+            eingabe[j] = eingabe[i];
+            j++;
+        }
+    }
+    while (j < sizeof(eingabe)) {
+        eingabe[j] = '\0';
+        j++;
+    }
+
+    //Erzeuge ein Array, dass genau so lang ist wie das Wort selbst
+    char bereinigt[strlen(eingabe) + 1];
+    char spiegel[strlen(eingabe) + 1];
+    strncpy(bereinigt, eingabe, strlen(eingabe) + 1);
+    strncpy(spiegel, eingabe, strlen(eingabe) + 1);
+
+    //Dieses Array spiegeln
+    for (int i = 0; i < strlen(spiegel) / 2; i++) {
+        char speicher = spiegel[i];
+        spiegel[i] = spiegel[strlen(spiegel) - 1 - i];
+        spiegel[strlen(spiegel) - 1 - i] = speicher;
+    }
+
+    //Vergleicht die beiden Arrays und findet Palindrome
+    if (strcmp(bereinigt, spiegel) == 0) {
+        printf("Palindrom!\n");
+    } else printf("Kein Palindrom!\n");
+
+    return 0;
+}

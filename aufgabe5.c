f38a5d314fe404a372cd9e17d583e5500cf28a4a
kevinstiebich <kevin.stiebich@gmail.com>
2026-07-24 13:24:53 +0200

    Kommentare hinzufügen

--- aufgabe5.c
+++ aufgabe5.c
@@ -7,7 +7,7 @@ int main() {
     printf("Gebe einen Satz ein: ");
     fgets(eingabe, sizeof(eingabe), stdin);
     
-    //Entfernen aller Zeichen außer die Buchstaben a-z und füllt den Rest mit Nullbytes
+    //entfernt alle Zeichen außer die Buchstaben a-z und füllt den Rest mit Nullbytes
     int j = 0;
     for (int i = 0; eingabe[i] != '\0'; i++) {
         if (eingabe[i] >= 'a' && eingabe[i] <= 'z') {
@@ -20,20 +20,20 @@ int main() {
         j++;
     }
 
-    //Erzeuge ein Array, dass genau so lang ist wie das Wort selbst
+    //erzeugt 2 Arrays, die genau so lang ist wie das Wort selbst und den relevanten Inhalt erben (+ ein Nullbyte)
     char bereinigt[strlen(eingabe) + 1];
     char spiegel[strlen(eingabe) + 1];
     strncpy(bereinigt, eingabe, strlen(eingabe) + 1);
     strncpy(spiegel, eingabe, strlen(eingabe) + 1);
 
-    //Dieses Array spiegeln
+    //eines dieser Arrays wird gespiegel
     for (int i = 0; i < strlen(spiegel) / 2; i++) {
         char speicher = spiegel[i];
         spiegel[i] = spiegel[strlen(spiegel) - 1 - i];
         spiegel[strlen(spiegel) - 1 - i] = speicher;
     }
 
-    //Vergleicht die beiden Arrays und findet Palindrome
+    //vergleicht die beiden Arrays, findet Palindrome und gibt das Ergebnis aus
     if (strcmp(bereinigt, spiegel) == 0) {
         printf("Palindrom!\n");
     } else printf("Kein Palindrom!\n");
